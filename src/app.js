813b7874b05bcd6402f13f7e97cc0175aabf1d1e
ditohafizh <ditohafizh@gmail.com>
2019-01-24 17:21:21 +0700

    video 6 : facebook auth (progress)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import graphqlHTTP from 'express-graphql';
+import passport from 'passport';
+import FacebookStrategy from 'passport-facebook';
 import schema from '../graphql';
+import {facebook} from './config';
+
+const transformFacebookProfile = (profile) => ({
+    name: profile.displayName,
+    avatar: profile.image.url,
+});
+
+passport.use(new FacebookStrategy(facebook,
+    async (accessToken, refreshToken, profile, done) => {
+        done(null, transformFacebookProfile(profile._json));
+    }
+));
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
 const app = express();
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {failureRedirect: '/auth/facebook'}),
+    (req, res) => res.redirect('AirbnbReactNativeClone://login?user=' + JSON.stringify(req.user))
+);
 
 app.use('/graphql', graphqlHTTP(req => ({
     schema,
---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+export const facebook = {
+    clientID: 'Facebook app id',
+    clientSecret: 'Facebook app secret id',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
